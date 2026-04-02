04f2c0ea07f441c462c5d814e62c264b4c7dab4c
matheus carvalho <matheusc0326@gmail.com>
2026-04-01 21:00:37 -0300

    aula 01/04

---
+++ javascript/aula 3/script.js
@@ -0,0 +1,77 @@
+const resultado = document.querySelector("#resultado");
+
+// let idade = parseInt(prompt('Digite sua idade'))
+// let carteira = confirm('Tem carteira de habilitação ?')
+// if (idade >= 18 && carteira){
+//   resultado.innerText = 'Pode dirigir'
+// }
+// else {
+//   resultado.innerText = 'Não pode dirigir'
+// }
+
+// let profissao = prompt("Digite sua profissão");
+// if(profissao == 'estudante' || profissao == 'professor'){
+//   resultado.innerText = 'Desconto concedido'
+// }
+// else{
+//   resultado.innerText = 'Desconto negado'
+// }
+
+// let idade = Number(prompt('Digite sua idade'))
+// let ingresso = confirm('Você possui ingresso')
+// if (idade >= 16 && ingresso) {
+//   resultado.innerText = "Pode entrar";
+// } else {
+//   resultado.innerText = "Não pode entrar";
+// }
+
+// let usuario = prompt('Digite sua usuario')
+// let senha = prompt('Digite sua senha')
+// if (usuario == "admin" && senha == "admin123") {
+//   resultado.innerText = "Logado";
+// } else {
+//   resultado.innerText = "Usuário não encontrado";
+// }
+
+// let valor = Number(prompt('Digite o valor do produto'))
+// let vip = confirm("Você é cliente vip?")
+// if (valor > 1000 || vip) {
+//   resultado.innerText = "Frete grátis e desconto concedidos";
+// } else {
+//   resultado.innerText = "Frete grátis e desconto não concedidos";
+// }
+
+// let senha = prompt('Digite sua senha')
+// if (senha !== "admin123") {
+//   resultado.innerText = "Acesso negado";
+// } else {
+//   resultado.innerText = "Logado com sucesso";
+// }
+
+// let idade = Number(prompt("Digite sua idade"));
+// let ingresso = confirm("Você possui ingresso?");
+// if (idade >= 16 && !ingresso) {
+//   resultado.innerText = "Precisa de convite";
+// } else if (idade >= 16) {
+//   resultado.innerText = "Entrada liberada";
+// } else {
+//   resultado.innerText = "Entrada proibida";
+// }
+
+// let valor = Number(prompt('Digite o valor do produto'))
+// let vip = confirm("Você é cliente vip?")
+// let cupom = confirm("Você possui cupom de desconto?")
+// if (valor >= 100 && (vip || cupom)) {
+//   resultado.innerText = "Desconto concedido";
+// } else {
+//   resultado.innerText = "Desconto negado";
+// }
+
+// let salario = Number(prompt("Digite o seu salário"));
+// let score = Number(prompt("Digite o seu score"));
+// let clienteAntigo = confirm("Você é cliente antigo?");
+// if (salario >= 2000 && (score >= 700 || (score >= 500 && clienteAntigo))) {
+//   resultado.innerText = "Emprestimo concedido";
+// } else {
+//   resultado.innerText = "Emprestimo negado";
+// }
